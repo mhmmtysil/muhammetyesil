417d1b896cb09fdd951c0784a17e6f9ec5388d65
mhmmtysil <muhammetyesil@outlook.com.tr>
2025-11-14 00:07:44 +0300

    publish

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  // Eğer alt dizinde yayınlayacaksanız (ör: /muhammetyesil/), aşağıyı ekleyin:
+  // basePath: '/muhammetyesil',
+  // assetPrefix: '/muhammetyesil',
 };
 
 export default nextConfig;
